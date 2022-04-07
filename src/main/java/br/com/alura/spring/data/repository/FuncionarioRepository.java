a410ff440dcb3c41a7e34cd2263011dde2bd1f37
Tuanemendes <tuane.belmiro@spdata.com.br>
2022-04-07 18:26:11 -0300

    Criação de projeções

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
 @Repository
 public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer>{
@@ -26,5 +26,7 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true )
 	List<Funcionario>findDataContratadaMaior(LocalDate data);
 	
+	@Query(value = "SELECT f.id,f.nome,f.salario FROM funcionarios f ",nativeQuery = true)
+	List<FuncionarioProjecao> findFuncionariSalario();
 	
 }
